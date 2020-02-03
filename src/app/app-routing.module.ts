81944fc2130f43946870b05074b866b02e9422ec
amine <jait.amine9@gmail.com>
2020-02-03 19:41:03 +0100

    added routers to login register and download to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UploadComponent } from './upload/upload.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  {path : 'upload', component : UploadComponent}
+  {path : 'upload', component : UploadComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'register', component : RegistrationComponent}
+
 ];
 
 @NgModule({
