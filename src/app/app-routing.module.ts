e1b2a8ee5b85ac7accdb67237cdeedb7f2a3b1a3
zineelabidin <bakherzineelabidine@gmail.com>
2020-04-19 17:18:58 +0000

    fix the probleme of routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { ManagerComponent } from './manager/manager.component';
 import { ResultComponent } from './result/result.component';
 
 const routes: Routes = [
-
+  { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
